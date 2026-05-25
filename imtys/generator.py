af9978e2a830c40d2d7610968a67e21d4036f72c
Why8484 <smilingorange11@gmail.com>
2026-05-25 17:45:34 +0300

    nklkmlk

---
+++ imtys/generator.py
@@ -0,0 +1,13 @@
+import random
+
+possible = [1,2,3,4,5,6,7,8,9]
+
+count = ""
+while type(count) != int:    
+    count = int(input("SKOLKO? "))
+
+output = []
+for _ in range(count):
+    output.append(random.choice(possible))
+
+print(output)
